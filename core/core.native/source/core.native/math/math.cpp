4938f4297027f3c63e886c5121ddd92d54a1c195
recuerdame <recuerdame@naver.com>
2015-10-08 09:32:01 +0900

    코딩컨벤션 변경
    코딩컨벤션 변경

--- core/core.native/source/core.native/math/math.cpp
+++ core/core.native/source/core.native/math/math.cpp
@@ -1,15 +1,21 @@
+//
+// Copyright (c) 2015. Lee eui jong.
+//
+// Distributed under the MIT License.
+//
+
 #include "math.h"
 
 namespace useless
 {
 	double math::PI = 3.14159265;
 
-	double math::RadianToDgree(double radian)
+	double math::rad_to_deg(double radian)
 	{
 		return radian * 180.0 / math::PI;
 	}
 
-	double math::DgreeToRadian(double degree)
+	double math::deg_to_rad(double degree)
 	{
 		return degree * math::PI / 180.0;
 	}
